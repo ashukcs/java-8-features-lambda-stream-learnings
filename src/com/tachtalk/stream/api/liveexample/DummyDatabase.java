a42319563c4da438b1dc32560a238c9ab51f0648
Ashutosh Kumar <ashutosh.kumar3@conduent.com>
2021-09-08 19:30:06 +0530

    commit code to demonstrate sorting using stream api

--- src/com/tachtalk/stream/api/liveexample/DummyDatabase.java
+++ src/com/tachtalk/stream/api/liveexample/DummyDatabase.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class DummyDatabase {
 	public static List<Employee> getEmployees(){
 		List<Employee> empList = new ArrayList<>();
-		empList.add(new Employee(1, "Suraj", "IT", 400000l));
-		empList.add(new Employee(1, "Sam", "IT", 800000l));
-		empList.add(new Employee(1, "Aman", "CIVIL", 600000l));
-		empList.add(new Employee(1, "Rohan", "MACHANICAL", 500000l));
+		empList.add(new Employee(123, "Suraj", "IT", 400000l));
+		empList.add(new Employee(145, "Sam", "IT", 800000l));
+		empList.add(new Employee(11, "Aman", "CIVIL", 600000l));
+		empList.add(new Employee(165, "Rohan", "MACHANICAL", 500000l));
 		
 		return empList;
 	}
